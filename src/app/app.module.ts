c1caa8220b7f7ea733665938dc03d129deda9a9c
Falk Franco <88750038+FalkFranco@users.noreply.github.com>
2022-07-12 01:39:54 -0300

    git commit -m "first commit"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavegacionComponent } from './components/navegacion/navegacion.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ArrowComponent } from './components/arrow/arrow.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { SocialComponent } from './components/social/social.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavegacionComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    ArrowComponent,
+    ProyectosComponent,
+    ProyectoComponent,
+    FooterComponent,
+    ContactoComponent,
+    ModalComponent,
+    SocialComponent,
   ],
   imports: [
     BrowserModule,
